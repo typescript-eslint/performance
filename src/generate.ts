67df2fdcee47bbd9ef74f29bc3752dedb8e1d2ce
Josh Goldberg <git@joshuakgoldberg.com>
2026-09-11 22:25:20 -0500

    lint removal

--- src/generate.ts
+++ src/generate.ts
@@ -4,13 +4,7 @@ import path from "node:path";
 
 import { createPackageFile } from "./creators/files/createPackageFile.js";
 import { writeCaseFiles } from "./creators/writeCaseFiles.js";
-import {
-	CaseData,
-	caseEntries,
-	casesPath,
-	localTypeScriptESLintPath,
-	NamedCaseData,
-} from "./data.js";
+import { CaseData, caseEntries, casesPath, NamedCaseData } from "./data.js";
 import { createProjectName } from "./utils.js";
 import { writeFile } from "./writing/writeFile.js";
 
